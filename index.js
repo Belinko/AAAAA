655192b98bc1715977ccea88220912533d5231f8
Eldar <Eldar@gmail.com>
2025-04-11 11:54:31 +0300

    Добавила вычитание

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const sum = (...args) => {
     return args.reduce((acc, val) => acc + val, 0);
   };
@@ -15,4 +14,8 @@ const sum = (...args) => {
   console.log(average(0, 10)); // 5
   console.log(average(-3, 4, 2, 10)); // 3.25
   console.log(average()); // null
+
+const difference = (a, b) => {
+  return a - b;
+}
 
